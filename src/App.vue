c231528d427209cd5314e56ff320b0dba283a363
Ego Lydia <94656569+EgoLydia@users.noreply.github.com>
2022-08-23 09:31:44 +0100

    create landing home page content

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-  </div>
-  <router-view/>
+  <div id="nav"><router-link to="/">Home</router-link> |</div>
+  <router-view />
 </template>
 
 <style>
+body {
+  padding: 0;
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
