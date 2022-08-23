b8479d870f327c64dbc9b126a179bf3421b15bed
Ego Lydia <94656569+EgoLydia@users.noreply.github.com>
2022-08-23 08:39:56 +0100

    create notelist view file

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,9 @@
 <template>
-  <single-note></single-note>
+  <div></div>
 </template>
 
 <script>
-import SingleNote from '@/components/SingleNote.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    SingleNote
-  }
-}
+  name: "Home",
+};
 </script>
