221b132271d4d19a1111f8703e8d6ea46944476a
Ego Lydia <94656569+EgoLydia@users.noreply.github.com>
2022-08-23 11:23:09 +0100

    Edit nav to contain different view links

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
+    <nav>
+      <div class="logo">
+        <p>CCA Frontend Cohort Note App</p>
+      </div>
+      <div class="links">
+        <router-link to="/">Home</router-link>
+        <router-link to="/notes">Notes</router-link>
+        <router-link to="/signup">Sign Up</router-link>
+        <router-link to="/login">Login</router-link>
+      </div>
+    </nav>
   </div>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
@@ -13,16 +23,29 @@
   text-align: center;
   color: #2c3e50;
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
+.logo {
   font-weight: bold;
   color: #2c3e50;
 }
-
+img {
+  max-width: 30px;
+}
+.links a {
+  margin-right: 30px;
+  text-decoration: none;
+  font-weight: bold;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+}
+nav {
+  max-width: 1120px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 auto;
+}
 #nav a.router-link-exact-active {
   color: #42b983;
 }
