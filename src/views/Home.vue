d3b9a24cec3293852d9ef0b9e50091b9e5c1046e
Ego Lydia <94656569+EgoLydia@users.noreply.github.com>
2022-08-26 10:54:08 +0100

    Removed text align from app vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,42 +7,9 @@ import LandingPage from "../components/LandingPage.vue";
 export default {
   name: "Home",
   components: {
-    LandingPage
-  }
+    LandingPage,
+  },
 };
-
 </script>
 
-<style scoped>
-.info {
-  display: flex;
-  margin-top: 16px;
-  gap: 25px;
-}
-h1 {
-  font-size: 36px;
-  width: 500px;
-  margin: 0 auto;
-}
-section {
-  margin-bottom: 64px;
-}
-.container {
-  max-width: 1120px;
-  margin: 0 auto;
-}
-a {
-  text-decoration: none;
-  color: #000;
-}
-button {
-  padding: 15px;
-  background-color: #e4fff3;
-  border-radius: 5px;
-  border: none;
-}
-footer {
-  background-color: #e4fff3;
-  padding: 20px;
-}
-</style>
+<style scoped></style>
