896b4303df7c778b4bb711b33cf62eae03965874
Ego Lydia <94656569+EgoLydia@users.noreply.github.com>
2022-08-26 15:17:14 +0100

    Update app vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,6 @@
 <template>
   <div id="nav">
-    <nav-menu>
-      <div class="logo">
-        <p>CCA Frontend Cohort Note App</p>
-      </div>
-      <div class="links">
-        <router-link to="/">Home</router-link>
-        <router-link to="/notes">Notes</router-link>
-        <router-link to="/SignupPage">Sign Up</router-link>
-        <router-link to="/LoginPage">Login</router-link>
-      </div>
-    </nav-menu>
+    <nav-menu></nav-menu>
   </div>
   <router-view />
 </template>
