5b01e70d1fb70c58a32c2bf514569f495b16748b
Ego Lydia <94656569+EgoLydia@users.noreply.github.com>
2022-08-26 12:50:56 +0100

    changed media query spec and align note to center

--- src/components/NotesCollection.vue
+++ src/components/NotesCollection.vue
@@ -1,49 +1,51 @@
 <template>
-    <div class="note-list">
-        <SingleNote v-for="(note, index) in notes" :key="index" :note="note" />
-    </div>
+  <div class="note-list">
+    <SingleNote v-for="(note, index) in notes" :key="index" :note="note" />
+  </div>
 </template>
 
 <script setup>
-import axios from 'axios'
-import SingleNote from '@/components/SingleNote.vue'
-import { onMounted, ref } from '@vue/runtime-core'
-import { BASE_URL } from '../config.js'
+import axios from "axios";
+import SingleNote from "@/components/SingleNote.vue";
+import { onMounted, ref } from "@vue/runtime-core";
+import { BASE_URL } from "../config.js";
 
-onMounted(()=>{
-    console.log('Notes Collection');
-    fetchNotes()
-})
-const notes = ref([])
-const fetchNotes = ()=>{
-    axios.get(`${BASE_URL}/Notes`)
-        .then((res) => {
-            notes.value = res.data
-        })
-        .catch(err => console.log(err))
-}
+onMounted(() => {
+  console.log("Notes Collection");
+  fetchNotes();
+});
+const notes = ref([]);
+const fetchNotes = () => {
+  axios
+    .get(`${BASE_URL}/Notes`)
+    .then(res => {
+      notes.value = res.data;
+    })
+    .catch(err => console.log(err));
+};
 </script>
 
 <style scoped>
-.page{
-    width:100%;
-    overflow: hidden;
+.page {
+  width: 100%;
+  overflow: hidden;
 }
-.note-list{
-    margin:1.5rem 0;
-    padding:1.5rem;
-    display: grid;
-    gap:1rem;
-    grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
-    justify-content: space-evenly;
-    
+.note-list {
+  margin: 1.5rem 0;
+  padding: 1.5rem;
+  display: grid;
+  gap: 1rem;
+  grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+  justify-content: space-evenly;
 }
 
-@media (min-width:630px) {
-    .note-list{
-        padding:2rem;
-        justify-content: start;
-        grid-template-columns: repeat(auto-fit, minmax(150px, 0.5fr));
-    }
+@media (min-width: 768px) {
+  .note-list {
+    padding: 2rem;
+    /* justify-content: start; */
+    margin: 0 auto;
+    grid-template-columns: repeat(auto-fit, minmax(150px, 0.5fr));
+    max-width: 1120px;
+  }
 }
 </style>
