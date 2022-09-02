3e445a6d16871b0504342b90e6f1d77f13e255f6
Samuel Jaja <samuel.jaja21@gmail.com>
2022-09-02 07:58:03 +0100

    Added post request for save note functionality

--- src/components/AddNotes.vue
+++ src/components/AddNotes.vue
@@ -21,30 +21,49 @@
     </div>
 
     <div class="btnSave">
-      <button class="save_button">Save Note</button>
+      <button type="submit" class="save_button">Save Note</button>
     </div>
   </form>
 </template>
 
 <script>
+// import { mapGetters, mapMutations, mapState, mapActions } from "vuex";
 import axios from "axios";
+
 export default {
   name: "AddNotes",
   data() {
     return {
       noteData: {
+        creatorUserId: "27dd9087-f8de-4ae3-a301-af03013e294c",
         title: "",
         content: "",
+        groupName: 0,
       },
     };
   },
-  method: {
+  computed: {
+    // ...mapGetters(["noteData"]),
+    // ...mapState(["noteData"]),
+  },
+  methods: {
+    // ...mapMutations(["setNoteData"]),
+    // ...mapActions(["getUserbyId", "createNote"]),
+
+    // async getUserbyId() {
+    //   const response = await axios.get(
+    //     "https://ccsanotes-api.azurewebsites.net/users/user/27dd9087-f8de-4ae3-a301-af03013e294c"
+    //   );
+    //   return response.data;
+    // },
+
     createNote() {
       axios
         .post(
           "https://ccsanotes-api.azurewebsites.net/Notes/create-note",
           this.noteData
         )
+
         .then((response) => console.log(response))
         .catch((error) => console.log(error));
     },
--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,51 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import axios from "axios";
 
 export default createStore({
-  state: {
-    //currentNote:{},
-  },
-  getters: {
-    getNoteById: (state) => (id) => {
-      return state.notes.find(note => note.id == id)
-    }
-  },
-  mutations: {
-    /*setCurrentNote(state,payload){
-      state.currentNote = state.notes.find(note=> note.id == payload)
-      console.log(state.currentNote)
-    }*/
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+    state: {
+        //currentNote:{},
+        noteData: {
+            userId: "",
+            title: "",
+            content: "",
+        },
+    },
+    getters: {
+        getNoteById: (state) => (id) => {
+            return state.notes.find((note) => note.id == id);
+        },
+
+        noteData: (state) => {
+            return state.noteData;
+        },
+    },
+    mutations: {
+        setNoteData(state, payload) {
+            state.noteData = payload;
+        },
+        /*setCurrentNote(state,payload){
+                                                      state.currentNote = state.notes.find(note=> note.id == payload)
+                                                      console.log(state.currentNote)
+                                                    }*/
+    },
+
+    actions: {
+        async getUserbyId() {
+            const response = await axios.get(
+                "https://ccsanotes-api.azurewebsites.net/users/user/27dd9087-f8de-4ae3-a301-af03013e294c"
+            );
+            return response.data;
+        },
+
+        async createNote() {
+            await axios
+                .post(
+                    "https://ccsanotes-api.azurewebsites.net/Notes/create-note",
+                    this.noteData
+                )
+                .then((response) => console.log(response))
+                .catch((error) => console.log(error));
+        },
+    },
+    modules: {},
+});
